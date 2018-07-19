417a3db3f22faaaa5fcb56b25b89c83e1b445339
Shehzad <shahzad@cacf.org>
2018-07-20 00:01:40 +0500

    Initial Setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://shezokhan:malakand10@ds121871.mlab.com:21871/mern_db'
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Body parser Middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI
+
+//Connect to Mongo
+mongoose
+    .connect(db)
+    .then( () => console.log("Mongo Connected") )
+    .catch(err => console.log(err));
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, ()=> console.log(`Listening to port ${port}`));
